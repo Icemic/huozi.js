401d19c834e2f7134b47ffd1aad1cf7f84700f6a
Icemic <bingfeng.jw@gmail.com>
2018-08-11 00:05:46 +0800

    add emoji example

--- example/demo.js
+++ example/demo.js
@@ -1,5 +1,5 @@
 import huozi from '../index';
-import { beiying, mixed, lorem } from './text';
+import { beiying, mixed, lorem, emoji } from './text';
 
 const canvas = document.getElementById('app');
 const context = canvas.getContext('2d');
@@ -9,8 +9,38 @@ const devicePixelRatio = window.devicePixelRatio;
 canvas.width = canvas.width * devicePixelRatio;
 canvas.height = canvas.height * devicePixelRatio;
 
+const regexAstralSymbols = /([\uD800-\uDBFF][\uDC00-\uDFFF])/;
+const specialForEmoji = ['\uFE0E', '\uFE0F', '\u200D', '\u{1F3FB}', '\u{1F3FC}', '\u{1F3FD}', '\u{1F3FE}', '\u{1F3FF}'];
+
+function processEmoji(arr) {
+  const retArr = [];
+  let pendingChar = '';
+  let lastIsSpecialForEmoji = false;
+  for (const char of arr) {
+    if (pendingChar && specialForEmoji.includes(char)) {
+      lastIsSpecialForEmoji = '\u200D' === char;
+      pendingChar += char;
+    } else if (pendingChar && lastIsSpecialForEmoji) {
+      lastIsSpecialForEmoji = false;
+      pendingChar += char;
+    } else if (pendingChar) {
+      retArr.push(pendingChar);
+      retArr.push(char);
+      pendingChar = '';
+    } else if (specialForEmoji.includes(char)) {
+      pendingChar = retArr.pop() + char;
+    } else if (regexAstralSymbols.test(char)) {
+      pendingChar = char;
+    } else {
+      retArr.push(char);
+    }
+  }
+
+  return retArr;
+}
+
 function drawText(text, options) {
-  const textSequence = text.replace(/\r\n/g, '\n').trim().split('').map(value => {
+  const textSequence = processEmoji(text.replace(/\r\n/g, '\n').trim()).map(value => {
     return {
       fontSize: +options.charFontSize || 18,
       character: value
@@ -62,8 +92,10 @@ function fillTextArea(num) {
     textBox.textContent = beiying;
   } else if (num === 2) {
     textBox.textContent = lorem;
-  } else {
+  } else if (num === 3) {
     textBox.textContent = mixed;
+  } else {
+    textBox.textContent = emoji;
   }
   handler();
 }
